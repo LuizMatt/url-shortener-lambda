87ab799893fbe3b74be2fd5bb13ad66ca11a6968
Matheus <luizmatheuslins5587@gmail.com>
2024-11-24 14:43:47 -0300

    Adicionando as pastas CreateUrlLambda e RedirectUrlShortner

---
+++ RedirectUrlShortner/src/main/java/com/rocketseat/redirectUrlShortner/UrlData.java
@@ -0,0 +1,31 @@
+package com.rocketseat.redirectUrlShortner;
+
+public class UrlData {
+
+    private String originalUrl;
+    private long expirationTime;
+
+    public UrlData(String originalUrl, long expirationTime) {
+        this.originalUrl = originalUrl;
+        this.expirationTime = expirationTime;
+    }
+    public UrlData() {
+    }
+
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public long getExpirationTime() {
+        return expirationTime;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public void setExpirationTime(long expirationTime) {
+        this.expirationTime = expirationTime;
+    }
+}
